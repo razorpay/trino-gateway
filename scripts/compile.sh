96dafd62f6f61ae1c94ab136f339e7b70fc2e948
Utkarsh Saxena <utkarsh.saxena@razorpay.com>
2021-09-26 02:19:03 +0530

    fixup! Fix build

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -4,4 +4,4 @@ go mod vendor
 go mod download
 
 protoc --go_out=. --twirp_out=. ./rpc/gateway/service.proto
-protoc --go_out=. --twirp_swagger_out=./third_party/swaggerui/service.swagger.json ./rpc/gateway/service.proto
+protoc --go_out=. --twirp_swagger_out=./third_party/swaggerui/ ./rpc/gateway/service.proto
