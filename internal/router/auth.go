08d5684514bda5cf520725f6f6e5ebe329ad85db
Utkarsh Saxena <utkarsh.saxena@razorpay.com>
2024-11-12 03:42:59 +0530

    add more users for auth test flow
    Signed-off-by: Utkarsh Saxena <utkarsh.saxena@razorpay.com>

--- internal/router/auth.go
+++ internal/router/auth.go
@@ -188,7 +188,9 @@ func (r *RouterServer) AuthHandler(ctx *context.Context, h http.Handler) http.Ha
 
 			// CustomAuth
 			if isBasicAuth {
-				if username == "upi-offering-service-payments.de-apps@razorpay.com" {
+				if username == "upi-offering-service-payments.de-apps@razorpay.com" ||
+					username == "reporting-service-platform-r1cxp.de-apps@razorpay.com" ||
+					username == "cross-border-import-service-payments.de-apps@razorpay.com" {
 					if u := trinoheaders.Get(trinoheaders.User, req); u != username {
 						errorMsg := fmt.Sprintf("Username from basicauth - %s does not match with User principal - %s", username, u)
 						provider.Logger(*ctx).Debug(errorMsg)
